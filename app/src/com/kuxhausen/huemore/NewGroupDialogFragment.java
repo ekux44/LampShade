428398969399bffefde9bacd3fefe994e38c2a58
ekux44 <ekux44@gmail.com>
2013-03-01 01:06:38 -0600

    group creation dialog UI completed

--- app/src/com/kuxhausen/huemore/NewGroupDialogFragment.java
+++ app/src/com/kuxhausen/huemore/NewGroupDialogFragment.java
@@ -7,35 +7,34 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class NewGroupDialogFragment extends DialogFragment {
 	
 	ArrayList mSelectedItems;
 	public static String[] dummyArrayItems = {"Bulb 1", "Bulb 2", "Bulb3"};
+	ListView bulbsListView;
 	
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         
-        mSelectedItems = new ArrayList();  // Where we track the selected items
-        builder.setMultiChoiceItems(dummyArrayItems, null,
-                new DialogInterface.OnMultiChoiceClickListener() {
-         @Override
-         public void onClick(DialogInterface dialog, int which,
-                 boolean isChecked) {
-             if (isChecked) {
-                 // If the user checked the item, add it to the selected items
-                 mSelectedItems.add(which);
-             } else if (mSelectedItems.contains(which)) {
-                 // Else, if the item is already in the array, remove it 
-                 mSelectedItems.remove(Integer.valueOf(which));
-             }
-         }
-     });
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View groupDialogView = inflater.inflate(R.layout.edit_group_dialog, null);
+        bulbsListView = ((ListView)groupDialogView.findViewById(R.id.listView1));
+        bulbsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        bulbsListView.setAdapter(new ArrayAdapter(this.getActivity(),android.R.layout.simple_list_item_multiple_choice,dummyArrayItems));
+        builder.setView(groupDialogView);
+        
         builder.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
-                       // FIRE ZE MISSILES!
+                       //bulbsListView.getCheckedItemPositions();
+                	   //bulbsListView.getCheckedItemIds();
                    }
                })
                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
