e20598a7ede0c0034b89b0731435dfec165b493d
ekux44 <ekux44@gmail.com>
2013-04-04 14:54:36 -0500

    Added bulb attributes api object
    Superset of bulb state

---
+++ app/src/com/kuxhausen/huemore/state/BulbAttributes.java
@@ -0,0 +1,11 @@
+package com.kuxhausen.huemore.state;
+
+public class BulbAttributes {
+	
+	public static BulbState state;
+	public String type;
+	public String name;
+	
+	public BulbAttributes() {
+	}
+}
