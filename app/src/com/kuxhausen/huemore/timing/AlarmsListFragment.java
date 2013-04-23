80ff5e1076c6a10a339e7a79838e3bf4f891b096
ekux44 <ekux44@gmail.com>
2013-04-23 02:08:10 -0500

    Scheduled alarms list shows json of alarms

--- app/src/com/kuxhausen/huemore/timing/AlarmsListFragment.java
+++ app/src/com/kuxhausen/huemore/timing/AlarmsListFragment.java
@@ -1,10 +1,13 @@
 package com.kuxhausen.huemore.timing;
 
 import com.kuxhausen.huemore.*;
+import com.kuxhausen.huemore.persistence.DatabaseDefinitions.AlarmColumns;
 
 import android.annotation.TargetApi;
+import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,14 +16,43 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 
-public class AlarmsListFragment extends ListFragment{
+public class AlarmsListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
+	// Identifies a particular Loader being used in this component
+	private static final int ALARMS_LOADER = 0;
+	public CursorAdapter dataSource;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// We need to use a different list item layout for devices older than
+				// Honeycomb
+				int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? android.R.layout.simple_list_item_activated_1
+						: android.R.layout.simple_list_item_1;
+
+				/*
+				 * Initializes the CursorLoader. The GROUPS_LOADER value is eventually
+				 * passed to onCreateLoader().
+				 */
+				getLoaderManager().initLoader(ALARMS_LOADER, null, this);
+
+				String[] columns = { AlarmColumns.STATE, BaseColumns._ID };
+				dataSource = new SimpleCursorAdapter(this.getActivity(), layout, null,
+						columns, new int[] { android.R.id.text1 }, 0);
+
+				setListAdapter(dataSource);
+		
+		
+		
+		
 		View myView = inflater.inflate(R.layout.alarm_view, null);
 		
 		LinearLayout headingRow = (LinearLayout) myView
@@ -63,5 +95,54 @@ public class AlarmsListFragment extends ListFragment{
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+
+
+	/**
+	 * Callback that's invoked when the system has initialized the Loader and is
+	 * ready to start the query. This usually happens when initLoader() is
+	 * called. The loaderID argument contains the ID value passed to the
+	 * initLoader() call.
+	 */
+	@Override
+	public Loader<Cursor> onCreateLoader(int loaderID, Bundle arg1) {
+		/*
+		 * Takes action based on the ID of the Loader that's being created
+		 */
+		switch (loaderID) {
+		case ALARMS_LOADER:
+			// Returns a new CursorLoader
+			String[] columns = { AlarmColumns.STATE, BaseColumns._ID };
+			return new CursorLoader(getActivity(), // Parent activity context
+					AlarmColumns.ALARMS_URI, // Table
+					columns, // Projection to return
+					null, // No selection clause
+					null, // No selection arguments
+					null // Default sort order
+			);
+		default:
+			// An invalid id was passed in
+			return null;
+		}
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
+		/*
+		 * Moves the query results into the adapter, causing the ListView
+		 * fronting this adapter to re-display
+		 */
+		dataSource.changeCursor(cursor);
+		registerForContextMenu(getListView());
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		/*
+		 * Clears out the adapter's reference to the Cursor. This prevents
+		 * memory leaks.
+		 */
+		// unregisterForContextMenu(getListView());
+		dataSource.changeCursor(null);
 	}
 }
