e281a78a893794ff25759e8361839fecb3521a0e
ekux44 <ekux44@gmail.com>
2013-06-03 00:14:39 -0700

    Created hooks for Tasker/Locale integration

---
+++ app/src/com/kuxhausen/huemore/automation/EditActivity.java
@@ -0,0 +1,7 @@
+package com.kuxhausen.huemore.automation;
+
+import android.app.Activity;
+
+public class EditActivity extends Activity {
+
+}
---
+++ app/src/com/kuxhausen/huemore/automation/FireReceiver.java
@@ -0,0 +1,15 @@
+package com.kuxhausen.huemore.automation;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class FireReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
