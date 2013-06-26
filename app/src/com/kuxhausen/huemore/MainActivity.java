6ea98f73492b65ff0c0c1e20646f70a295c1a034
Eric Kuxhausen <eric@drchronos-MacBook-Pro.local>
2013-06-26 15:48:00 -0700

    reorganized MainActivity to issolate billing and app initialization

--- app/src/com/kuxhausen/huemore/MainActivity.java
+++ app/src/com/kuxhausen/huemore/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends GodObject implements
 	private MainActivity m;
 	Inventory lastQuerriedInventory;
 	public GetBulbList.OnBulbListReturnedListener bulbListenerFragment;
-	private NfcAdapter nfcAdapter;
 	
 	public void setBulbListenerFragment(GetBulbList.OnBulbListReturnedListener frag){
 		bulbListenerFragment = frag;
@@ -56,8 +55,6 @@ public class MainActivity extends GodObject implements
 
 		setContentView(R.layout.hue_more);
 		m = this;
-		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
 		// Check whether the activity is using the layout version with
 		// the fragment_container FrameLayout. If so, we must add the first
 		// fragment
@@ -91,34 +88,9 @@ public class MainActivity extends GodObject implements
 		}
 
 		
-		runDatabaseInitializationChecks();
-		// (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) ?
-		// this.getActionBar().setDisplayHomeAsUpEnabled(true)
-
-		String firstChunk = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgPUhHgGEdnpyPMAWgP3Xw/jHkReU1O0n6d4rtcULxOrVl/hcZlOsVyByMIZY5wMD84gmMXjbz8pFb4RymFTP7Yp8LSEGiw6DOXc7ydNd0lbZ4WtKyDEwwaio1wRbRPxdU7/4JBpMCh9L6geYx6nYLt0ExZEFxULV3dZJpIlEkEYaNGk/64gc0l34yybccYfORrWzu8u+";
-		String secondChunk = "5YxJ5k1ikIJJ2I7/2Rp5AXkj2dWybmT+AGx83zh8+iMGGawEQerGtso9NUqpyZWU08EO9DcF8r2KnFwjmyWvqJ2JzbqCMNt0A08IGQNOrd16/C/65GE6J/EtsggkNIgQti6jD7zd3b2NAQIDAQAB";
-		String base64EncodedPublicKey = firstChunk + secondChunk;
-		// compute your public key and store it in base64EncodedPublicKey
-		mPlayHelper = new IabHelper(this, base64EncodedPublicKey);
-		Log.d("asdf", "mPlayHelperCreated" + (mPlayHelper != null));
-		mPlayHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-			@Override
-			public void onIabSetupFinished(IabResult result) {
-				if (!result.isSuccess()) {
-					// Oh noes, there was a problem.
-					// Log.d("asdf", "Problem setting up In-app Billing: "+
-					// result);
-				} else {
-					// Hooray, IAB is fully set up!
-					mPlayHelper.queryInventoryAsync(mGotInventoryListener);
-					if (m.bulbListenerFragment != null) {
-						GetBulbList pushGroupMood = new GetBulbList(m,
-								m.bulbListenerFragment, m);
-						pushGroupMood.execute();
-					}
-				}
-			}
-		});
+		initializationDatabaseChecks();
+		initializeBillingCode();
+		
 
 		Bundle b = this.getIntent().getExtras();
 		if (b != null && b.containsKey(InternalArguments.PROMPT_UPGRADE)
@@ -127,202 +99,6 @@ public class MainActivity extends GodObject implements
 			unlocks.show(getSupportFragmentManager(),
 					InternalArguments.FRAG_MANAGER_DIALOG_TAG);
 		}
-	}
-	
-	private void runDatabaseInitializationChecks(){
-		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-		if (!settings.contains(PreferencesKeys.TWO_POINT_TWO_UPDATE)) {
-			databaseHelper.updatedTwoPointOnePointOne();
-			// Run this before all the other updates, as it launches the update
-			// dialog, which needs to know update history
-			if (settings.contains(PreferencesKeys.TWO_POINT_OH_UPDATE)
-					&& (settings.getInt(PreferencesKeys.BULBS_UNLOCKED,
-							PreferencesKeys.ALWAYS_FREE_BULBS) > PreferencesKeys.ALWAYS_FREE_BULBS)) {
-				VersionHistoryDialogFragment vhdf = new VersionHistoryDialogFragment();
-				vhdf.show(getSupportFragmentManager(),
-						InternalArguments.FRAG_MANAGER_DIALOG_TAG);
-			}
-			// Mark no longer update two point two in preferences cache
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.TWO_POINT_TWO_UPDATE, false);
-			edit.commit();
-		}
-
-		if (!settings.contains(PreferencesKeys.FIRST_RUN)) {
-			databaseHelper.initialPopulate();// initialize database
-
-			// Mark no longer first run in preferences cache
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.FIRST_RUN, false);
-			edit.putInt(PreferencesKeys.BULBS_UNLOCKED,
-					PreferencesKeys.ALWAYS_FREE_BULBS);// TODO load from
-			// google store
-			edit.commit();
-		}
-		if (!settings.contains(PreferencesKeys.THIRD_UPDATE)) {
-			databaseHelper.updatedPopulate();
-			// Mark no longer first update in preferences cache
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.THIRD_UPDATE, false);
-			edit.commit();
-		}
-		if (!settings.contains(PreferencesKeys.TWO_POINT_OH_UPDATE)) {
-			databaseHelper.updatedTwoPointOh();
-			// Mark no longer first update in preferences cache
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.TWO_POINT_OH_UPDATE, false);
-			edit.commit();
-		}
-		if (!settings.contains(PreferencesKeys.TWO_POINT_ONE_UPDATE)) {
-			databaseHelper.updatedTwoPointOne();
-			// Mark no longer first update in preferences cache
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.TWO_POINT_ONE_UPDATE, false);
-			edit.commit();
-		}
-		if (!settings.contains(PreferencesKeys.TWO_POINT_ONE_POINT_ONE_UPDATE)) {
-			databaseHelper.updatedTwoPointOnePointOne();
-			// Mark no longer first update in preferences cache
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.TWO_POINT_ONE_POINT_ONE_UPDATE,
-					false);
-			edit.commit();
-		}
-
-		if (!settings.contains(PreferencesKeys.DEFAULT_TO_GROUPS)) {
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.DEFAULT_TO_GROUPS, false);
-			edit.commit();
-		}
-		if (!settings.contains(PreferencesKeys.DEFAULT_TO_MOODS)) {
-			Editor edit = settings.edit();
-			edit.putBoolean(PreferencesKeys.DEFAULT_TO_MOODS, true);
-			edit.commit();
-		}
-
-		// check to see if the bridge IP address is setup yet
-		if (!settings.contains(PreferencesKeys.BRIDGE_IP_ADDRESS)) {
-			DiscoverHubDialogFragment dhdf = new DiscoverHubDialogFragment();
-			dhdf.show(this.getSupportFragmentManager(),
-					InternalArguments.FRAG_MANAGER_DIALOG_TAG);
-		}
-	}
-
-	// Listener that's called when we finish querying the items and
-	// subscriptions we own
-	IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-		@Override
-		public void onQueryInventoryFinished(IabResult result,
-				Inventory inventory) {
-
-			// Log.d("asdf", "Query inventory finished.");
-			if (result.isFailure()) {
-				// handle error
-				return;
-			} else {
-				// Log.d("asdf", "Query inventory was successful.");
-				lastQuerriedInventory = inventory;
-				int numUnlocked = PreferencesKeys.ALWAYS_FREE_BULBS;
-				if (inventory.hasPurchase(PlayItems.FIVE_BULB_UNLOCK_1))
-					numUnlocked = Math.max(50, numUnlocked);
-				if (inventory.hasPurchase(PlayItems.BUY_ME_A_BULB_DONATION_1))
-					numUnlocked = Math.max(50, numUnlocked);
-				// update UI accordingly
-
-				// Get preferences cache
-				SharedPreferences settings = PreferenceManager
-						.getDefaultSharedPreferences(m);
-				int previousMax = settings.getInt(
-						PreferencesKeys.BULBS_UNLOCKED,
-						PreferencesKeys.ALWAYS_FREE_BULBS);
-				if (numUnlocked > previousMax) {
-					// Update the number held in settings
-					Editor edit = settings.edit();
-					edit.putInt(PreferencesKeys.BULBS_UNLOCKED, numUnlocked);
-					edit.commit();
-
-					databaseHelper.addBulbs(previousMax, numUnlocked);// initialize
-																		// database
-				}
-			}
-			/*
-			 * Check for items we own. Notice that for each purchase, we check
-			 * the developer payload to see if it's correct! See
-			 * verifyDeveloperPayload().
-			 */
-			/*
-			 * // Do we have the premium upgrade? Purchase premiumPurchase =
-			 * inventory.getPurchase(SKU_PREMIUM); mIsPremium = (premiumPurchase
-			 * != null && verifyDeveloperPayload(premiumPurchase)); Log.d(TAG,
-			 * "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
-			 * 
-			 * 
-			 * updateUi(); setWaitScreen(false); Log.d(TAG,
-			 * "Initial inventory query finished; enabling main UI.");
-			 */
-		}
-	};
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + ","
-		// + data);
-
-		// Pass on the activity result to the helper for handling
-		if (!mPlayHelper.handleActivityResult(requestCode, resultCode, data)) {
-			// not handled, so handle it ourselves (here's where you'd
-			// perform any handling of activity results not related to in-app
-			// billing...
-			super.onActivityResult(requestCode, resultCode, data);
-		} else {
-			// Log.d(TAG, "onActivityResult handled by IABUtil.");
-		}
-	}
-
-	/** Verifies the developer payload of a purchase. */
-	boolean verifyDeveloperPayload(Purchase p) {
-		String payload = p.getDeveloperPayload();
-
-		/*
-		 * TODO: verify that the developer payload of the purchase is correct.
-		 * It will be the same one that you sent when initiating the purchase.
-		 * 
-		 * WARNING: Locally generating a random string when starting a purchase
-		 * and verifying it here might seem like a good approach, but this will
-		 * fail in the case where the user purchases an item on one device and
-		 * then uses your app on a different device, because on the other device
-		 * you will not have access to the random string you originally
-		 * generated.
-		 * 
-		 * So a good developer payload has these characteristics:
-		 * 
-		 * 1. If two different users purchase an item, the payload is different
-		 * between them, so that one user's purchase can't be replayed to
-		 * another user.
-		 * 
-		 * 2. The payload must be such that you can verify it even when the app
-		 * wasn't the one who initiated the purchase flow (so that items
-		 * purchased by the user on one device work on other devices owned by
-		 * the user).
-		 * 
-		 * Using your own server to store and verify developer payloads across
-		 * app installations is recommended.
-		 */
-
-		return true;
-	}
-
-	@Override
-	public void onDestroy() {
-		if (mPlayHelper != null) {
-			try {
-				mPlayHelper.dispose();
-			} catch (IllegalArgumentException e) {
-			}
-		}
-		mPlayHelper = null;
-		Log.d("asdf", "mPlayHelperDestroyed" + (mPlayHelper == null));
-		super.onDestroy();
 	}
 
 	@Override
@@ -410,7 +186,7 @@ public class MainActivity extends GodObject implements
 				unlocksItem.setEnabled(false);
 				unlocksItem.setVisible(false);
 			}
-			if (nfcAdapter == null) {
+			if (NfcAdapter.getDefaultAdapter(this) == null) {
 				// hide nfc link if nfc not supported
 				MenuItem nfcItem = menu.findItem(R.id.action_nfc);
 				if (nfcItem != null) {
@@ -473,7 +249,7 @@ public class MainActivity extends GodObject implements
 					InternalArguments.FRAG_MANAGER_DIALOG_TAG);
 			return true;
 		case R.id.action_nfc:
-			if (!nfcAdapter.isEnabled()) {
+			if (!NfcAdapter.getDefaultAdapter(this).isEnabled()) {
 				// startActivity(new
 				// Intent(SettingsDialogFragment.ACTION_NFC_SETTINGS));
 				Toast.makeText(this, this.getString(R.string.nfc_disabled),
@@ -492,5 +268,225 @@ public class MainActivity extends GodObject implements
 			return super.onOptionsItemSelected(item);
 		}
 	}
+	
+	
+	@Override
+	public void onDestroy() {
+		if (mPlayHelper != null) {
+			try {
+				mPlayHelper.dispose();
+			} catch (IllegalArgumentException e) {
+			}
+		}
+		mPlayHelper = null;
+		Log.d("asdf", "mPlayHelperDestroyed" + (mPlayHelper == null));
+		super.onDestroy();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + ","
+		// + data);
 
+		// Pass on the activity result to the helper for handling
+		if (!mPlayHelper.handleActivityResult(requestCode, resultCode, data)) {
+			// not handled, so handle it ourselves (here's where you'd
+			// perform any handling of activity results not related to in-app
+			// billing...
+			super.onActivityResult(requestCode, resultCode, data);
+		} else {
+			// Log.d(TAG, "onActivityResult handled by IABUtil.");
+		}
+	}
+	
+	private void initializationDatabaseChecks(){
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+		if (!settings.contains(PreferencesKeys.TWO_POINT_TWO_UPDATE)) {
+			databaseHelper.updatedTwoPointOnePointOne();
+			// Run this before all the other updates, as it launches the update
+			// dialog, which needs to know update history
+			if (settings.contains(PreferencesKeys.TWO_POINT_OH_UPDATE)
+					&& (settings.getInt(PreferencesKeys.BULBS_UNLOCKED,
+							PreferencesKeys.ALWAYS_FREE_BULBS) > PreferencesKeys.ALWAYS_FREE_BULBS)) {
+				VersionHistoryDialogFragment vhdf = new VersionHistoryDialogFragment();
+				vhdf.show(getSupportFragmentManager(),
+						InternalArguments.FRAG_MANAGER_DIALOG_TAG);
+			}
+			// Mark no longer update two point two in preferences cache
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.TWO_POINT_TWO_UPDATE, false);
+			edit.commit();
+		}
+
+		if (!settings.contains(PreferencesKeys.FIRST_RUN)) {
+			databaseHelper.initialPopulate();// initialize database
+
+			// Mark no longer first run in preferences cache
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.FIRST_RUN, false);
+			edit.putInt(PreferencesKeys.BULBS_UNLOCKED,
+					PreferencesKeys.ALWAYS_FREE_BULBS);// TODO load from
+			// google store
+			edit.commit();
+		}
+		if (!settings.contains(PreferencesKeys.THIRD_UPDATE)) {
+			databaseHelper.updatedPopulate();
+			// Mark no longer first update in preferences cache
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.THIRD_UPDATE, false);
+			edit.commit();
+		}
+		if (!settings.contains(PreferencesKeys.TWO_POINT_OH_UPDATE)) {
+			databaseHelper.updatedTwoPointOh();
+			// Mark no longer first update in preferences cache
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.TWO_POINT_OH_UPDATE, false);
+			edit.commit();
+		}
+		if (!settings.contains(PreferencesKeys.TWO_POINT_ONE_UPDATE)) {
+			databaseHelper.updatedTwoPointOne();
+			// Mark no longer first update in preferences cache
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.TWO_POINT_ONE_UPDATE, false);
+			edit.commit();
+		}
+		if (!settings.contains(PreferencesKeys.TWO_POINT_ONE_POINT_ONE_UPDATE)) {
+			databaseHelper.updatedTwoPointOnePointOne();
+			// Mark no longer first update in preferences cache
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.TWO_POINT_ONE_POINT_ONE_UPDATE,
+					false);
+			edit.commit();
+		}
+
+		if (!settings.contains(PreferencesKeys.DEFAULT_TO_GROUPS)) {
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.DEFAULT_TO_GROUPS, false);
+			edit.commit();
+		}
+		if (!settings.contains(PreferencesKeys.DEFAULT_TO_MOODS)) {
+			Editor edit = settings.edit();
+			edit.putBoolean(PreferencesKeys.DEFAULT_TO_MOODS, true);
+			edit.commit();
+		}
+
+		// check to see if the bridge IP address is setup yet
+		if (!settings.contains(PreferencesKeys.BRIDGE_IP_ADDRESS)) {
+			DiscoverHubDialogFragment dhdf = new DiscoverHubDialogFragment();
+			dhdf.show(this.getSupportFragmentManager(),
+					InternalArguments.FRAG_MANAGER_DIALOG_TAG);
+		}
+	}
+
+	private void initializeBillingCode(){
+		String firstChunk = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgPUhHgGEdnpyPMAWgP3Xw/jHkReU1O0n6d4rtcULxOrVl/hcZlOsVyByMIZY5wMD84gmMXjbz8pFb4RymFTP7Yp8LSEGiw6DOXc7ydNd0lbZ4WtKyDEwwaio1wRbRPxdU7/4JBpMCh9L6geYx6nYLt0ExZEFxULV3dZJpIlEkEYaNGk/64gc0l34yybccYfORrWzu8u+";
+		String secondChunk = "5YxJ5k1ikIJJ2I7/2Rp5AXkj2dWybmT+AGx83zh8+iMGGawEQerGtso9NUqpyZWU08EO9DcF8r2KnFwjmyWvqJ2JzbqCMNt0A08IGQNOrd16/C/65GE6J/EtsggkNIgQti6jD7zd3b2NAQIDAQAB";
+		String base64EncodedPublicKey = firstChunk + secondChunk;
+		// compute your public key and store it in base64EncodedPublicKey
+		mPlayHelper = new IabHelper(this, base64EncodedPublicKey);
+		Log.d("asdf", "mPlayHelperCreated" + (mPlayHelper != null));
+		mPlayHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+			@Override
+			public void onIabSetupFinished(IabResult result) {
+				if (!result.isSuccess()) {
+					// Oh noes, there was a problem.
+					// Log.d("asdf", "Problem setting up In-app Billing: "+
+					// result);
+				} else {
+					// Hooray, IAB is fully set up!
+					mPlayHelper.queryInventoryAsync(mGotInventoryListener);
+					if (m.bulbListenerFragment != null) {
+						GetBulbList pushGroupMood = new GetBulbList(m,
+								m.bulbListenerFragment, m);
+						pushGroupMood.execute();
+					}
+				}
+			}
+		});
+	}
+	
+	// Listener that's called when we finish querying the items and subscriptions we own
+	IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+		@Override
+		public void onQueryInventoryFinished(IabResult result,
+				Inventory inventory) {
+
+			// Log.d("asdf", "Query inventory finished.");
+			if (result.isFailure()) {
+				// handle error
+				return;
+			} else {
+				// Log.d("asdf", "Query inventory was successful.");
+				lastQuerriedInventory = inventory;
+				int numUnlocked = PreferencesKeys.ALWAYS_FREE_BULBS;
+				if (inventory.hasPurchase(PlayItems.FIVE_BULB_UNLOCK_1))
+					numUnlocked = Math.max(50, numUnlocked);
+				if (inventory.hasPurchase(PlayItems.BUY_ME_A_BULB_DONATION_1))
+					numUnlocked = Math.max(50, numUnlocked);
+				// update UI accordingly
+
+				// Get preferences cache
+				SharedPreferences settings = PreferenceManager
+						.getDefaultSharedPreferences(m);
+				int previousMax = settings.getInt(
+						PreferencesKeys.BULBS_UNLOCKED,
+						PreferencesKeys.ALWAYS_FREE_BULBS);
+				if (numUnlocked > previousMax) {
+					// Update the number held in settings
+					Editor edit = settings.edit();
+					edit.putInt(PreferencesKeys.BULBS_UNLOCKED, numUnlocked);
+					edit.commit();
+
+					databaseHelper.addBulbs(previousMax, numUnlocked);// initialize
+																		// database
+				}
+			}
+			/*
+			 * Check for items we own. Notice that for each purchase, we check
+			 * the developer payload to see if it's correct! See
+			 * verifyDeveloperPayload().
+			 */
+			/*
+			 * // Do we have the premium upgrade? Purchase premiumPurchase =
+			 * inventory.getPurchase(SKU_PREMIUM); mIsPremium = (premiumPurchase
+			 * != null && verifyDeveloperPayload(premiumPurchase)); Log.d(TAG,
+			 * "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+			 * 
+			 * 
+			 * updateUi(); setWaitScreen(false); Log.d(TAG,
+			 * "Initial inventory query finished; enabling main UI.");
+			 */
+		}
+	};
+
+	/** Verifies the developer payload of a purchase. */
+	boolean verifyDeveloperPayload(Purchase p) {
+		String payload = p.getDeveloperPayload();
+		/*
+		 * TODO: verify that the developer payload of the purchase is correct.
+		 * It will be the same one that you sent when initiating the purchase.
+		 * 
+		 * WARNING: Locally generating a random string when starting a purchase
+		 * and verifying it here might seem like a good approach, but this will
+		 * fail in the case where the user purchases an item on one device and
+		 * then uses your app on a different device, because on the other device
+		 * you will not have access to the random string you originally
+		 * generated.
+		 * 
+		 * So a good developer payload has these characteristics:
+		 * 
+		 * 1. If two different users purchase an item, the payload is different
+		 * between them, so that one user's purchase can't be replayed to
+		 * another user.
+		 * 
+		 * 2. The payload must be such that you can verify it even when the app
+		 * wasn't the one who initiated the purchase flow (so that items
+		 * purchased by the user on one device work on other devices owned by
+		 * the user).
+		 * 
+		 * Using your own server to store and verify developer payloads across
+		 * app installations is recommended.
+		 */
+		return true;
+	}
 }
