d223477e27605804aaec41363a6fe4264b8d82aa
Eric Kuxhausen <eric@drchronos-MacBook-Pro.local>
2013-08-09 10:19:03 -0700

    Let event implement time based comparable

--- app/src/com/kuxhausen/huemore/state/Event.java
+++ app/src/com/kuxhausen/huemore/state/Event.java
@@ -2,11 +2,16 @@ package com.kuxhausen.huemore.state;
 
 import com.kuxhausen.huemore.state.api.BulbState;
 
-public class Event {
+public class Event implements Comparable<Event>{
 	public BulbState state;
 	/**in units of 1/10 of a second */
 	public Integer time;
 	/** 0 indexed **/
 	public Integer channel;
+	
+	@Override
+	public int compareTo(Event another) {
+		return time.compareTo(another.time);
+	}
 	
 }
