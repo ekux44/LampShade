8cb2b4d5497c6b59bda4b443408a56fd450cc8ba
ekux44 <ekux44@gmail.com>
2013-08-28 09:44:11 -0500

    Added text to decode error activity.. now for nav

--- app/src/com/kuxhausen/huemore/DecodeErrorActivity.java
+++ app/src/com/kuxhausen/huemore/DecodeErrorActivity.java
@@ -1,20 +1,26 @@
 package com.kuxhausen.huemore;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.kuxhausen.huemore.persistence.DatabaseDefinitions.InternalArguments;
 
 public class DecodeErrorActivity extends SherlockActivity {
 
+	TextView messageText;
+	
 	@Override
 	public void onCreate(Bundle b){
 		setContentView(R.layout.decoder_error_activity);
+		
+		messageText = (TextView)findViewById(R.id.messageTextView);
+		
 		boolean bool = getIntent().getExtras().getBoolean(InternalArguments.DECODER_ERROR_UPGRADE);
 		if(bool){
-			
+			messageText.setText(R.string.update_required);
 		} else{
-			
+			messageText.setText(R.string.corrupted_mood);
 		}
 	}
 }
