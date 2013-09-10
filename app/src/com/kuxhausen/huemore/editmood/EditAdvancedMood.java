e73457288d6eafc7184caf47ae21c8bc79ea105b
ekux44 <ekux44@gmail.com>
2013-09-10 02:30:26 -0500

    Looks like grid layout does support dynamic resize
    but something in the mood row adapter is broken and should be replaced
    in edit advanced mood

--- app/src/com/kuxhausen/huemore/editmood/EditAdvancedMood.java
+++ app/src/com/kuxhausen/huemore/editmood/EditAdvancedMood.java
@@ -13,23 +13,37 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.support.v7.widget.GridLayout;
 import android.support.v7.widget.GridLayout.LayoutParams;
 import android.support.v7.widget.GridLayout.Spec;
 
-public class EditAdvancedMood extends Activity implements OnItemClickListener {
+public class EditAdvancedMood extends Activity implements OnClickListener {
 
 	GridLayout grid;
 	
 	MoodRowAdapter rayAdapter;
 	ArrayList<MoodRow> moodRowArray;
+	Button addChannel, addTimeslot;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_advanced_mood);
+		
+		addTimeslot = (Button) this.findViewById(R.id.addTimeslotButton);
+		addTimeslot.setOnClickListener(this);
+		
+		addChannel = (Button) this.findViewById(R.id.addChannelButton);
+		//int h = addChannel.getHeight();
+		//int w = addChannel.getWidth();
+		//addChannel.setRotation(270);
+		//addChannel.setHeight(w);
+		//addChannel.setWidth(h);
+		addChannel.setOnClickListener(this);
 		
 		grid = (GridLayout) this.findViewById(R.id.advancedGridLayout);
 		grid.setColumnCount(2);
@@ -44,6 +58,8 @@ public class EditAdvancedMood extends Activity implements OnItemClickListener {
 	    addState();
 	    addState();
 	    addState();
+	    
+	    redrawGrid();
 	}
 	
 	private void addState() {
@@ -54,8 +70,15 @@ public class EditAdvancedMood extends Activity implements OnItemClickListener {
 		mr.hs = example;
 		moodRowArray.add(mr);
 		rayAdapter.add(mr);
-		
-		populateGrid(rayAdapter.getCount()-1);
+	}
+	private void addState(int i) {
+		MoodRow mr = new MoodRow();
+		mr.color = 0xff000000;
+		BulbState example = new BulbState();
+		example.on = false;
+		mr.hs = example;
+		moodRowArray.add(i, mr);
+		rayAdapter.insert(mr, i);
 	}
 	
 	private void populateGrid(int index){
@@ -65,18 +88,47 @@ public class EditAdvancedMood extends Activity implements OnItemClickListener {
 		
 		grid.addView(rayAdapter.getView(index, null, grid), vg);
 	}
-
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.edit_advanced_mood, menu);
 		return true;
 	}
-
+	
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		// TODO Auto-generated method stub
-		
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.addChannelButton:
+			int width = grid.getColumnCount();
+			grid.setColumnCount(1+width);
+			for(int i = rayAdapter.getCount(); i>0; i-=width){
+				addState(i);
+			}
+			redrawGrid();
+			break;
+		case R.id.addTimeslotButton:
+			grid.setRowCount(grid.getRowCount()+1);
+			for(int i = grid.getColumnCount(); i>0; i--){
+				addState();
+			}
+			redrawGrid();
+			break;
+		}
 	}
 
+	private void redrawGrid() {
+		grid.removeAllViews();
+		for(int r = 0; r< grid.getRowCount(); r++)
+			for(int c = 0; c<grid.getColumnCount(); c++){
+				GridLayout.LayoutParams vg = new GridLayout.LayoutParams();
+				vg.columnSpec = GridLayout.spec(c);
+				vg.rowSpec = GridLayout.spec(r);
+				
+				grid.addView(rayAdapter.getView(0, null, grid), vg);
+			}
+		
+		grid.invalidate();
+	}
 }
