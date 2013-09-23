6a44add563a99146e9c1357c7dfbb0a6044e051f
ekux44 <ekux44@gmail.com>
2013-09-23 13:38:21 -0500

    Simple States Fragment now works

--- app/src/com/kuxhausen/huemore/editmood/SampleStatesFragment.java
+++ app/src/com/kuxhausen/huemore/editmood/SampleStatesFragment.java
@@ -2,9 +2,11 @@ package com.kuxhausen.huemore.editmood;
 
 import java.util.ArrayList;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,12 +17,22 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.google.gson.Gson;
+import com.kuxhausen.huemore.GodObject;
 import com.kuxhausen.huemore.R;
 import com.kuxhausen.huemore.editmood.EditStatePagerDialogFragment.OnCreateColorListener;
+import com.kuxhausen.huemore.persistence.DatabaseDefinitions.InternalArguments;
+import com.kuxhausen.huemore.persistence.Utils;
+import com.kuxhausen.huemore.state.Mood;
 import com.kuxhausen.huemore.state.api.BulbState;
 
 public class SampleStatesFragment extends SherlockFragment implements OnCreateColorListener, OnClickListener{
 
+	private GridView g;
+	private View lastSelection;
+	private MoodRow lastSelectedRow;
+	Gson gson = new Gson();
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -55,23 +67,43 @@ public class SampleStatesFragment extends SherlockFragment implements OnCreateCo
 		}
 		
 		
-		GridView g = (GridView) myView.findViewById(R.id.myGrid);
+		g = (GridView) myView.findViewById(R.id.myGrid);
 		g.setAdapter(new ImageAdapter(this, list));
+		
+		//set up an initial selection
+		//onClick(lastSelectedRow.getView(0, null, this, this));
 		
 		return myView;
 	}
 	
 	@Override
 	public Intent onCreateColor(Integer transitionTime) {
-		// TODO Auto-generated method stub
-		return null;
+		lastSelectedRow.hs.transitiontime = transitionTime;
+		Intent i = new Intent();
+		i.putExtra(InternalArguments.HUE_STATE, gson.toJson(lastSelectedRow.hs));
+		i.putExtra(InternalArguments.COLOR, lastSelectedRow.color);
+		return i;
 	}
 	
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		int position = (Integer) v.getTag();
+
+		v.setBackgroundColor(0xFFFFBB33);
+		if(lastSelection!=null)
+			lastSelection.setBackgroundColor(0);
 		
+		lastSelection = v;
+		lastSelectedRow = (MoodRow)g.getAdapter().getItem(position);
+		preview();
+	}
+	
+	public void preview() {
+		if(lastSelectedRow!=null){
+			Mood m = Utils.generateSimpleMood(lastSelectedRow.hs);	
+			Utils.transmit(this.getActivity(), InternalArguments.ENCODED_TRANSIENT_MOOD, m, ((GodObject) getActivity()).getBulbs(), null);
+		}
 	}
 
 	
@@ -86,36 +118,17 @@ public class SampleStatesFragment extends SherlockFragment implements OnCreateCo
         }
 
         public Object getItem(int position) {
-            return position;
+            return list.get(position);
         }
 
         public long getItemId(int position) {
             return position;
         }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-        	
-        	
+        public View getView(int position, View convertView, ViewGroup parent) {        	
         	View v = list.get(position).getView(position, parent, l, SampleStatesFragment.this);
-        	v.setLayoutParams(new GridView.LayoutParams(100, 100));
-        	v.setPadding(8, 8, 8, 8);
         	
         	return v;
-        	/*
-        	ImageView imageView;
-            if (convertView == null) {
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(72, 72));
-                imageView.setAdjustViewBounds(false);
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(8, 8, 8, 8);
-            } else {
-                imageView = (ImageView) convertView;
-            }
-
-            imageView.setImageResource(mThumbIds[position]);
-			
-            return imageView;*/
         }
 
         private OnClickListener l;
