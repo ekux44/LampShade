2120a6f04cb43da18a922660e869e421cb338453
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-02-28 00:23:30 -0600

    cleaned up NFC reader in prep for rerouting through service

--- app/src/com/kuxhausen/huemore/nfc/NfcReaderActivity.java
+++ app/src/com/kuxhausen/huemore/nfc/NfcReaderActivity.java
@@ -2,12 +2,10 @@ package com.kuxhausen.huemore.nfc;
 
 import java.nio.charset.Charset;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Pair;
@@ -21,27 +19,23 @@ import android.widget.ToggleButton;
 import com.actionbarsherlock.view.MenuItem;
 import com.kuxhausen.huemore.DecodeErrorActivity;
 import com.kuxhausen.huemore.MainActivity;
-import com.kuxhausen.huemore.MoodExecuterService;
 import com.kuxhausen.huemore.NetworkManagedSherlockFragmentActivity;
 import com.kuxhausen.huemore.R;
-import com.kuxhausen.huemore.network.NetworkMethods;
 import com.kuxhausen.huemore.persistence.DatabaseDefinitions.InternalArguments;
 import com.kuxhausen.huemore.persistence.FutureEncodingException;
 import com.kuxhausen.huemore.persistence.HueUrlEncoder;
 import com.kuxhausen.huemore.persistence.InvalidEncodingException;
 import com.kuxhausen.huemore.persistence.Utils;
-import com.kuxhausen.huemore.state.Event;
 import com.kuxhausen.huemore.state.Mood;
 import com.kuxhausen.huemore.state.api.BulbState;
 
-public class NfcReaderActivity extends NetworkManagedSherlockFragmentActivity implements
-		OnCheckedChangeListener, OnClickListener {
+public class NfcReaderActivity extends NetworkManagedSherlockFragmentActivity implements OnCheckedChangeListener, OnClickListener {
 
-	Integer[] bulbS;
-	Mood m;
-	Integer brightness;
-	ToggleButton onButton;
-	Button doneButton;
+	private Integer[] mBulbs;
+	private Mood mood;
+	private Integer mBrightness;
+	private ToggleButton mOnButton;
+	private Button mDoneButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +43,10 @@ public class NfcReaderActivity extends NetworkManagedSherlockFragmentActivity im
 		setContentView(R.layout.nfc_reader);
 		this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		
-		onButton = (ToggleButton) this.findViewById(R.id.onToggleButton);
+		mOnButton = (ToggleButton) this.findViewById(R.id.onToggleButton);
 
-		doneButton = (Button) this.findViewById(R.id.doneButton);
-		doneButton.setOnClickListener(this);
+		mDoneButton = (Button) this.findViewById(R.id.doneButton);
+		mDoneButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -60,64 +54,63 @@ public class NfcReaderActivity extends NetworkManagedSherlockFragmentActivity im
 		super.onResume();
 
 		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-			Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(
-					NfcAdapter.EXTRA_NDEF_MESSAGES);
-			if (rawMsgs != null) {
-				NdefMessage[] msgs = new NdefMessage[rawMsgs.length];
-				for (int i = 0; i < rawMsgs.length; i++) {
-					msgs[i] = (NdefMessage) rawMsgs[i];
-				}
-				// msgs[0].
+			Pair<Integer[], Pair<Mood, Integer>> result = NfcReaderActivity.getGroupMoodBrightnessFromNdef(this, getIntent());
+			if(result==null){
+				this.finish();
+				return;
+			}
+			mBulbs = result.first;
+			mood = result.second.first;
+			mBrightness = result.second.second;
+			
+			mOnButton.setOnCheckedChangeListener(null);
+			Utils.transmit(this, mood, mBulbs, null, null, mBrightness);
+			boolean on = false;
+			if(mood.events[0].state.on!=null && mood.events[0].state.on)
+				on=true;
+			mOnButton.setChecked(on);
+			mOnButton.setOnCheckedChangeListener(this);
+		}
+	}
+	
+	public static Pair<Integer[],Pair<Mood,Integer>> getGroupMoodBrightnessFromNdef(Context c, Intent input){
+		Parcelable[] rawMsgs = input.getParcelableArrayExtra(
+				NfcAdapter.EXTRA_NDEF_MESSAGES);
+		if (rawMsgs != null) {
+			NdefMessage[] msgs = new NdefMessage[rawMsgs.length];
+			for (int i = 0; i < rawMsgs.length; i++) {
+				msgs[i] = (NdefMessage) rawMsgs[i];
+			}
+			
+			byte[] payload = msgs[0].getRecords()[0].getPayload();
+			
+			String data = new String(payload, 1, payload.length - 1,
+					Charset.forName("US-ASCII"));
+			data = data.substring(data.indexOf('?') + 1);
 
-				byte[] payload = msgs[0].getRecords()[0].getPayload();
-				byte identifierCode = payload[0];
-
-				// String prefix = (0x01)+""; // you need to implement this one
-				// String url = prefix +
-				// new String(payload, 1, payload.length -1,
-				// Charset.forName("US-ASCII"));
-
-				String data = new String(payload, 1, payload.length - 1,
-						Charset.forName("US-ASCII"));
-				data = data.substring(data.indexOf('?') + 1);
-
-				try {
-					Pair<Integer[], Pair<Mood, Integer>> result = HueUrlEncoder.decode(data);
-					bulbS = result.first;
-					m = result.second.first;
-					brightness = result.second.second;
-					
-					onButton.setOnCheckedChangeListener(null);
-					Utils.transmit(this, m, bulbS, null, null, brightness);
-					boolean on = false;
-					if(m.events[0].state.on!=null && m.events[0].state.on)
-						on=true;
-					onButton.setChecked(on);
-					onButton.setOnCheckedChangeListener(this);
-				} catch (InvalidEncodingException e) {
-					Intent i = new Intent(this,DecodeErrorActivity.class);
-					i.putExtra(InternalArguments.DECODER_ERROR_UPGRADE, false);
-					startActivity(i);
-					this.finish();
-				} catch (FutureEncodingException e) {
-					Intent i = new Intent(this,DecodeErrorActivity.class);
-					i.putExtra(InternalArguments.DECODER_ERROR_UPGRADE, true);
-					startActivity(i);
-					this.finish();
-				}
+			try {
+				return HueUrlEncoder.decode(data);
+			} catch (InvalidEncodingException e) {
+				Intent i = new Intent(c,DecodeErrorActivity.class);
+				i.putExtra(InternalArguments.DECODER_ERROR_UPGRADE, false);
+				c.startActivity(i);
+			} catch (FutureEncodingException e) {
+				Intent i = new Intent(c,DecodeErrorActivity.class);
+				i.putExtra(InternalArguments.DECODER_ERROR_UPGRADE, true);
+				c.startActivity(i);
 			}
 		}
+		return null;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
 		switch (item.getItemId()) {
-		case android.R.id.home:
-			this.startActivity(new Intent(this,MainActivity.class));
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
+			case android.R.id.home:
+				this.startActivity(new Intent(this,MainActivity.class));
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 		}
 	}
 
@@ -126,7 +119,7 @@ public class NfcReaderActivity extends NetworkManagedSherlockFragmentActivity im
 		BulbState bs = new BulbState();
 		bs.on = isChecked;
 		Mood m = Utils.generateSimpleMood(bs);		
-		Utils.transmit(this, m, bulbS, null, null, brightness);
+		Utils.transmit(this, m, mBulbs, null, null, mBrightness);
 	}
 
 	@Override
