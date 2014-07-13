3ecd51be3ed43fc2e2f2a1ada55c2126277bfbb2
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-07-13 01:40:52 -0700

    Started working on platform specific conversion functions

---
+++ mobile/src/main/java/com/kuxhausen/huemore/net/lifx/LifxConversions.java
@@ -0,0 +1,14 @@
+package com.kuxhausen.huemore.net.lifx;
+
+public class LifxConversions {
+
+  /**
+   * @param bri in 0-100
+   * @return bri in 0.01 - 1.0
+   */
+  public static float dbBriToLifxBri(int bri){
+    bri = Math.max(1, Math.min(100, bri));
+    return bri/100f;
+  }
+
+}
