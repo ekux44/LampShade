1fda8af2ac5e0aa4b831289f549aa6223c836947
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-07-27 21:57:37 -0700

    created a testing playground

---
+++ mobile/src/androidTest/java/com/kuxhausen/huemore/TestingPlayground.java
@@ -0,0 +1,19 @@
+package com.kuxhausen.huemore;
+
+import android.test.AndroidTestCase;
+
+public class TestingPlayground extends AndroidTestCase {
+
+
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testAdd(){
+    assertNull(4);
+  }
+}
