17e95818d35826f24a1f08bba61e4074c3c47a90
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-11-17 15:41:58 -0600

    Alarms logic now passes first test

--- mobile/src/androidTest/java/com/kuxhausen/huemore/AlarmLogicTest.java
+++ mobile/src/androidTest/java/com/kuxhausen/huemore/AlarmLogicTest.java
@@ -4,46 +4,52 @@ import android.test.AndroidTestCase;
 
 import com.kuxhausen.huemore.alarm.AlarmData;
 import com.kuxhausen.huemore.alarm.AlarmLogic;
-import com.kuxhausen.huemore.alarm.DaysOfWeek;
 
 import java.util.Calendar;
 
 public class AlarmLogicTest extends AndroidTestCase {
 
+  private Calendar yesterday, now, oneHourForward, oneDayForward, twoDaysForward;
+
   protected void setUp() throws Exception {
     super.setUp();
+
+    yesterday = Calendar.getInstance();
+    yesterday.clear();
+    yesterday.set(2014, Calendar.JULY, 25, 13, 44);
+
+    now = Calendar.getInstance();
+    now.clear();
+    now.set(2014, Calendar.JULY, 26, 13, 44);
+
+    oneHourForward = Calendar.getInstance();
+    oneHourForward.clear();
+    oneHourForward.set(2014, Calendar.JULY, 26, 14, 44);
+
+    oneDayForward = Calendar.getInstance();
+    oneDayForward.clear();
+    oneDayForward.set(2014, Calendar.JULY, 27, 13, 44);
+
+    twoDaysForward = Calendar.getInstance();
+    twoDaysForward.clear();
+    twoDaysForward.set(2014, Calendar.JULY, 28, 13, 44);
+
   }
 
   protected void tearDown() throws Exception {
     super.tearDown();
   }
 
-  public void testComputeNextAlarmTime(){
+  public void testComputeNextAlarmTime1() {
 
-    Calendar yesterday = Calendar.getInstance();
-    yesterday.set(2014, Calendar.JULY, 26, 13, 44);
-
-    Calendar now = Calendar.getInstance();
-    now.set(2014, Calendar.JULY, 25, 13, 44);
-
-    Calendar oneHourForward = Calendar.getInstance();
-    oneHourForward.set(2014, Calendar.JULY, 27, 14, 44);
-
-    Calendar oneDayForward = Calendar.getInstance();
-    oneDayForward.set(2014, Calendar.JULY, 27, 13, 44);
-
-    Calendar twoDaysForward = Calendar.getInstance();
-    twoDaysForward.set(2014, Calendar.JULY, 27, 13, 44);
-
-    DaysOfWeek noRepeats = new DaysOfWeek();
     AlarmData ad1 = new AlarmData(-1);
     ad1.setEnabled(true);
     ad1.setNextTime(oneDayForward.getTimeInMillis());
-    ad1.setHour(14);
+    ad1.setHour(13);
     ad1.setMinute(44);
 
-    assertEquals((Long)oneDayForward.getTimeInMillis(), AlarmLogic.computeNextAlarmTime(ad1, now.getTimeInMillis()));
-
+    assertEquals((Long) oneDayForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, now.getTimeInMillis()));
 
   }
 }
