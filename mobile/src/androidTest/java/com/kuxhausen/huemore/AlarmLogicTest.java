8542ee8c3d1ff909c877ae3484e0fa6979de6e94
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-11-19 06:10:56 -0600

    Implemented & tested repeating days alarm logic

--- mobile/src/androidTest/java/com/kuxhausen/huemore/AlarmLogicTest.java
+++ mobile/src/androidTest/java/com/kuxhausen/huemore/AlarmLogicTest.java
@@ -4,16 +4,22 @@ import android.test.AndroidTestCase;
 
 import com.kuxhausen.huemore.alarm.AlarmData;
 import com.kuxhausen.huemore.alarm.AlarmLogic;
+import com.kuxhausen.huemore.alarm.DaysOfWeek;
 
 import java.util.Calendar;
 
 public class AlarmLogicTest extends AndroidTestCase {
 
-  private Calendar yesterday, now, oneMinForward, oneHourForward, oneDayForward,
-      oneDayOneMinForward, twoDaysForward;
+  private Calendar twoDaysPrior, yesterday, now, oneMinForward, oneHourForward, oneDayForward,
+      oneDayOneMinForward, twoDaysForward, sixDaysForward, sevenDaysForward, eightDaysForward,
+      thirteenDaysForward;
 
   protected void setUp() throws Exception {
     super.setUp();
+
+    twoDaysPrior = Calendar.getInstance();
+    twoDaysPrior.clear();
+    twoDaysPrior.set(2014, Calendar.JULY, 24, 13, 44);
 
     yesterday = Calendar.getInstance();
     yesterday.clear();
@@ -21,7 +27,7 @@ public class AlarmLogicTest extends AndroidTestCase {
 
     now = Calendar.getInstance();
     now.clear();
-    now.set(2014, Calendar.JULY, 26, 13, 44);
+    now.set(2014, Calendar.JULY, 26, 13, 44); // this is a Saturday
 
     oneMinForward = Calendar.getInstance();
     oneMinForward.clear();
@@ -43,6 +49,21 @@ public class AlarmLogicTest extends AndroidTestCase {
     twoDaysForward.clear();
     twoDaysForward.set(2014, Calendar.JULY, 28, 13, 44);
 
+    sixDaysForward = Calendar.getInstance();
+    sixDaysForward.clear();
+    sixDaysForward.set(2014, Calendar.AUGUST, 1, 13, 44);
+
+    sevenDaysForward = Calendar.getInstance();
+    sevenDaysForward.clear();
+    sevenDaysForward.set(2014, Calendar.AUGUST, 2, 13, 44);
+
+    eightDaysForward = Calendar.getInstance();
+    eightDaysForward.clear();
+    eightDaysForward.set(2014, Calendar.AUGUST, 3, 13, 44);
+
+    thirteenDaysForward = Calendar.getInstance();
+    thirteenDaysForward.clear();
+    thirteenDaysForward.set(2014, Calendar.AUGUST, 8, 13, 44);
   }
 
   protected void tearDown() throws Exception {
@@ -53,7 +74,6 @@ public class AlarmLogicTest extends AndroidTestCase {
 
     AlarmData ad1 = new AlarmData(-1);
     ad1.setEnabled(false);
-    ad1.setNextTime(oneDayForward.getTimeInMillis());
     ad1.setHour(oneDayForward.get(Calendar.HOUR_OF_DAY));
     ad1.setMinute(oneDayForward.get(Calendar.MINUTE));
 
@@ -64,7 +84,6 @@ public class AlarmLogicTest extends AndroidTestCase {
 
     AlarmData ad1 = new AlarmData(-1);
     ad1.setEnabled(true);
-    ad1.setNextTime(oneDayForward.getTimeInMillis());
     ad1.setHour(oneDayForward.get(Calendar.HOUR_OF_DAY));
     ad1.setMinute(oneDayForward.get(Calendar.MINUTE));
 
@@ -77,7 +96,6 @@ public class AlarmLogicTest extends AndroidTestCase {
 
     AlarmData ad1 = new AlarmData(-1);
     ad1.setEnabled(true);
-    ad1.setNextTime(oneMinForward.getTimeInMillis());
     ad1.setHour(oneMinForward.get(Calendar.HOUR_OF_DAY));
     ad1.setMinute(oneMinForward.get(Calendar.MINUTE));
 
@@ -90,12 +108,72 @@ public class AlarmLogicTest extends AndroidTestCase {
 
     AlarmData ad1 = new AlarmData(-1);
     ad1.setEnabled(true);
-    ad1.setNextTime(oneHourForward.getTimeInMillis());
     ad1.setHour(oneHourForward.get(Calendar.HOUR_OF_DAY));
     ad1.setMinute(oneHourForward.get(Calendar.MINUTE));
 
     assertEquals((Long) oneHourForward.getTimeInMillis(),
                  AlarmLogic.computeNextAlarmTime(ad1, now.getTimeInMillis()));
 
+  }
+
+  public void testComputeNextAlarmTime4() {
+
+    AlarmData ad1 = new AlarmData(-1);
+    ad1.setEnabled(true);
+    ad1.setHour(yesterday.get(Calendar.HOUR_OF_DAY));
+    ad1.setMinute(yesterday.get(Calendar.MINUTE));
+
+    assertEquals((Long) oneDayForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, now.getTimeInMillis()));
+  }
+
+  public void testComputeNextAlarmTime1Repeating() {
+
+    DaysOfWeek fridays = new DaysOfWeek();
+    fridays.setDay(Calendar.FRIDAY, true);
+
+    AlarmData ad1 = new AlarmData(-1);
+    ad1.setEnabled(true);
+    ad1.setHour(yesterday.get(Calendar.HOUR_OF_DAY));
+    ad1.setMinute(yesterday.get(Calendar.MINUTE));
+    ad1.setRepeatDays(fridays);
+
+    assertEquals((Long) sixDaysForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, now.getTimeInMillis()));
+
+    assertEquals((Long) sixDaysForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, oneDayOneMinForward.getTimeInMillis()));
+
+    assertEquals((Long) yesterday.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, twoDaysPrior.getTimeInMillis()));
+
+    assertEquals((Long) thirteenDaysForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, sixDaysForward.getTimeInMillis()));
+
+  }
+
+  public void testComputeNextAlarmTime2Repeating() {
+
+    DaysOfWeek weekends = new DaysOfWeek();
+    weekends.setDay(Calendar.SATURDAY, true);
+    weekends.setDay(Calendar.SUNDAY, true);
+
+    AlarmData ad1 = new AlarmData(-1);
+    ad1.setEnabled(true);
+    ad1.setHour(yesterday.get(Calendar.HOUR_OF_DAY));
+    ad1.setMinute(yesterday.get(Calendar.MINUTE));
+    ad1.setRepeatDays(weekends);
+
+    assertEquals((Long) now.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, yesterday.getTimeInMillis()));
+
+    assertEquals((Long) oneDayForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, now.getTimeInMillis()));
+
+    assertEquals((Long) sevenDaysForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, oneDayOneMinForward.getTimeInMillis()));
+
+    assertEquals((Long) eightDaysForward.getTimeInMillis(),
+                 AlarmLogic.computeNextAlarmTime(ad1, sevenDaysForward.getTimeInMillis()));
   }
 }
--- mobile/src/main/java/com/kuxhausen/huemore/alarm/AlarmLogic.java
+++ mobile/src/main/java/com/kuxhausen/huemore/alarm/AlarmLogic.java
@@ -1,5 +1,6 @@
 package com.kuxhausen.huemore.alarm;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class AlarmLogic {
@@ -7,12 +8,12 @@ public class AlarmLogic {
 
   public static Long computeNextAlarmTime(AlarmData alarmData, long currentUnixTime) {
     if (alarmData.isEnabled()) {
+
+      Calendar now = Calendar.getInstance();
+      now.setTimeInMillis(currentUnixTime);
+      now.add(Calendar.MINUTE, 1); // jump ahead 1 minute to disallow alarm going off immediately
+
       if (alarmData.getRepeatDays().isNoDaysSet()) {
-
-        Calendar now = Calendar.getInstance();
-        now.setTimeInMillis(currentUnixTime);
-        now.add(Calendar.MINUTE, 1); // jump ahead 1 minute to disallow alarm going off immediately
-
         Calendar alarmTime = Calendar.getInstance();
         alarmTime.setTimeInMillis(currentUnixTime);
         alarmTime.set(Calendar.HOUR_OF_DAY, alarmData.getHour());
@@ -20,14 +21,55 @@ public class AlarmLogic {
         alarmTime.set(Calendar.MINUTE, alarmData.getMinute());
         alarmTime.getTimeInMillis(); // force internals fields to be recomputed after set
 
-        if (!alarmTime.after(now)) {
+        while (!alarmTime.after(now)) {
           alarmTime.add(Calendar.DAY_OF_YEAR, 1);
         }
 
         return alarmTime.getTimeInMillis();
+
+      } else {
+        ArrayList<Long> repeatingDays = new ArrayList<Long>();
+
+        for (int dayNo = 1; dayNo <= 7; dayNo++) {
+          if (alarmData.getRepeatDays().isDaySet(dayNo)) {
+
+            Calendar alarmTime = Calendar.getInstance();
+            alarmTime.setTimeInMillis(currentUnixTime);
+
+            alarmTime.set(Calendar.DAY_OF_WEEK, dayNo);
+            alarmTime.getTimeInMillis(); // force internals fields to be recomputed after set
+
+            alarmTime.set(Calendar.HOUR_OF_DAY, alarmData.getHour());
+            alarmTime.getTimeInMillis(); // force internals fields to be recomputed after set
+            alarmTime.set(Calendar.MINUTE, alarmData.getMinute());
+            alarmTime.getTimeInMillis(); // force internals fields to be recomputed after set
+
+            //move adjust this alarmTime such that it is the first occurrence of this day of week after now
+            {
+              while (!alarmTime.before(now)) {
+                alarmTime.add(Calendar.WEEK_OF_YEAR, -1);
+              }
+              while (!alarmTime.after(now)) {
+                alarmTime.add(Calendar.WEEK_OF_YEAR, 1);
+              }
+            }
+
+            repeatingDays.add(alarmTime.getTimeInMillis());
+          }
+        }
+        //now return the earliest valid day
+        if (repeatingDays.size() > 0) {
+          long result = Long.MAX_VALUE;
+          for (long dayTime : repeatingDays) {
+            if (dayTime < result) {
+              result = dayTime;
+            }
+          }
+          return result;
+        }
+
       }
     }
-
     return null;
   }
 }
