eaba1cfb7e579d2ec11fa71d9124cadfacb5be2c
Eric Kuxhausen <ekux@google.com>
2015-11-07 13:54:12 -0800

    Pre-ICS Preference crash fix
    Fixes #94

--- mobile/src/main/java/com/kuxhausen/huemore/persistence/PreferenceInitializer.java
+++ mobile/src/main/java/com/kuxhausen/huemore/persistence/PreferenceInitializer.java
@@ -7,7 +7,7 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.Cursor;
-import android.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceManager;
 import android.provider.BaseColumns;
 
 import com.kuxhausen.huemore.CommunityDialogFragment;
