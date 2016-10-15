303ffcc9881070b80f29b71081d2a1d66682c507
Eric Kuxhausen <ekux@google.com>
2016-10-15 16:07:09 -0700

    Null-pointer fix in DeferredLog

--- mobile/src/main/java/com/kuxhausen/huemore/utils/DeferredLog.java
+++ mobile/src/main/java/com/kuxhausen/huemore/utils/DeferredLog.java
@@ -6,7 +6,7 @@ import com.kuxhausen.huemore.BuildConfig;
 
 public class DeferredLog {
 
-  public final static boolean IS_LOGGABLE = BuildConfig.DEBUG;
+  private final static boolean IS_LOGGABLE = BuildConfig.DEBUG;
 
   public static boolean isLoggable() {
     return IS_LOGGABLE;
@@ -14,30 +14,35 @@ public class DeferredLog {
 
   public static void d(String tag, String format, Object... args) {
     if (isLoggable()) {
+      format = (format != null) ? format : "null";
       Log.d(tag, String.format(format, args));
     }
   }
 
   public static void e(String tag, String format, Object... args) {
     if (isLoggable()) {
+      format = (format != null) ? format : "null";
       Log.d(tag, String.format(format, args));
     }
   }
 
   public static void i(String tag, String format, Object... args) {
     if (isLoggable()) {
+      format = (format != null) ? format : "null";
       Log.d(tag, String.format(format, args));
     }
   }
 
   public static void v(String tag, String format, Object... args) {
     if (isLoggable()) {
+      format = (format != null) ? format : "null";
       Log.d(tag, String.format(format, args));
     }
   }
 
   public static void w(String tag, String format, Object... args) {
     if (isLoggable()) {
+      format = (format != null) ? format : "null";
       Log.d(tag, String.format(format, args));
     }
   }
